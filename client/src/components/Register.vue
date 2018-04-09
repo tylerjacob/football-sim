8e2be1ca5b19da6241609c08e42e7c7d096897b3
Tyler Mckee-langer <tyler@Tylers-MacBook-Pro.local>
2018-04-09 19:27:41 -0400

    added vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,24 +1,43 @@
 <template>
-  <div>
-<h1>Register</h1>
-<input
-type="email"
-name="email"
-v-model="email"
-placeholder="email" >
-
-<br>
-<input
-type="password"
-name="password"
-v-model="password"
-placeholder="password" >
-<br>
-<button
-@click="register">
-Register
-</button>
-  </div>
+    
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs10>
+            <v-card flat class="elevation-10">
+              <v-toolbar dark color="cyan">
+                <v-spacer></v-spacer>
+                <v-toolbar-title dark>Enter Login Information</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                </v-tooltip>
+              </v-toolbar>
+                <div class="pr-4 pl-4 pt-2 pb-2">
+                    <input
+                    type="email"
+                    name="email"
+                    v-model="email"
+                    placeholder="email" >
+                    <br>
+                    <input
+                    type="password"
+                    name="password"
+                    v-model="password"
+                    placeholder="password" >
+                    <br>
+                    <div class="error" v-html="error"/>
+                    <br>
+                    <v-spacer></v-spacer>
+                  <v-btn @click="register" color="cyan">Login</v-btn>
+                </div>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+       
 </template>
 
 <script>
@@ -27,16 +46,20 @@ export default {
   data () {
     return {
       email: 'abc@gmail.com',
-      password: 'password'
+      password: 'password',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -45,5 +68,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error {
+  color: red;
+}
+.cards {
+  padding: 0 0 10em 10em
+}
 </style>
