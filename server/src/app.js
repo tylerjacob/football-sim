2ec34017b2b1c9b66cbd7253e79f8b12a3c0ce69
Tyler Mckee-langer <tyler@Tylers-MacBook-Pro.local>
2018-04-08 23:50:14 -0400

    setting up login page

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+console.log('Hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: 'register page!'
+  })
+})
+
+console.log('listening on port 8081!')
+app.listen(process.env.PORT || 8081)
